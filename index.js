c561d1b81e5c088cf9dbe552caabc3c3cc309208
Kunalraj270 <kunalraj2999@gmail.com>
2022-01-19 20:42:08 +0530

    set up out view engine

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const port = 8000;
 //Use express router
 app.use('/',require('./routes/index'));
 
+//set up the view engine
 
+app.set('view engine' , 'ejs');
+app.set('views' , './views')
 
 
 app.listen(port , function(err){
