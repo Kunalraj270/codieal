2a361938476ae2f03470d6bb94b80b15cd6e7ed3
Kunalraj270 <kunalraj2999@gmail.com>
2022-01-19 21:26:22 +0530

    add views for home and profile

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const homeController = require('../controllers/home_controllers');
 
 console.log('Router loaded');
 router.get('/' , homeController.home);
-router.use('/users' , require('./users'));
+router.use('/users/' , require('./users'));
 // For any further routes access from here
 router.use('/post' , require('./post'));
 
