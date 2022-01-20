00799e8660c6bbdc81670ca0e1c51b2b4d1fe48c
Kunalraj270 <kunalraj2999@gmail.com>
2022-01-21 01:47:06 +0530

    install mongoose and set up mongoDb for database

---
+++ config/mongoos.js
@@ -0,0 +1,20 @@
+//require the libarary
+const mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb://localhost/codieal_development');
+
+//aquire the connection (to check it is sucessfull)
+const db = mongoose.connection;
+
+//error handling
+
+db.on('error' , console.error.bind(console , 'error connecting to db'));
+
+//up and running then print the message
+
+db.once('open' , function(){
+    console.log('Connected to database :: MongoDb');
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//import db
+const db = require('./config/mongoos')
 //layouts
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
