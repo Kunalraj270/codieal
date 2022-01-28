0661254f944a4fc2fe62de6125c12db786f2f29b
Kunalraj270 <kunalraj2999@gmail.com>
2022-01-28 18:47:04 +0530

    Authentication on post

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 
 
 // post controller
 const postsController = require('../controllers/post_controllers');
 
-router.post('/create' , postsController.create);
+router.post('/create' , passport.checkAuthentication, postsController.create);
 
 
 
