c4efe7a3ab69d7628cd883f91023725cd9a7b726
Kunalraj270 <kunalraj2999@gmail.com>
2022-02-11 16:22:49 +0530

    set up passport for google Oauth2 startgey

---
+++ config/passport-google-oauth2-stratgey.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const googleSratergy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+// tell passprot to use new startgy for google sign in 
+passport.use(new googleSratergy({
+    clientID: "802198501272-4msfqb0q6d06a64af4k11b6hi3lstg5e.apps.googleusercontent.com",
+    clientSecret: "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",
+    callbackURL: "http://localhost:8000/users/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, done) {
+        // finding the user
+        User.findOne({ emails: profile.emails[0].value }).exec(function (err, user) {
+            if (err) {
+                console.log('Error in google passprot-stratgey', err);
+                return;
+            }
+            console.log(profile);
+            // if user found then req.user
+            if (user) {
+                return done(null, user);
+                // if not found then create a user in database
+            } else {
+                User.create({
+                    name: profile.displayName,
+                    email: profile.email[0].value,
+                    // crypto is use to generate random password
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function (err) {
+                    if (err) {
+                        console.log('Error in google passprot-stratgey', err);
+                        return;
+                    }
+                    return done(null , user);
+                });
+            }
+        });
+    }
+));
+
+
+module.exports = passport;
