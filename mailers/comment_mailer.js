1b6ce412a05c2704388233412a633f9498a8e749
Kunalraj270 <kunalraj2999@gmail.com>
2022-02-14 23:25:35 +0530

    sending email via SMTP with template email

--- mailers/comment_mailer.js
+++ mailers/comment_mailer.js
@@ -4,13 +4,13 @@ const nodeMailer = require('../config/nodemailer');
 // this is another way of exporting a method
 exports.newComment = (comment) => {
     console.log('inside newComment mailer', comment);
-    // let htmlString = nodeMailer.renderTemplate({comment : comment} , './comments/new_comment.ejs')
+    let htmlString = nodeMailer.renderTemplate({comment : comment} , './comments/new_comment.ejs')
 
     nodeMailer.transporter.sendMail({
        from: 'kunalraj2999@gmail.com',
        to: 'kunalraj2999@gmail.com',
        subject: "New Comment Published!",
-       html: "htmlString" 
+       html: htmlString
     }, (err, info) => {
         if (err){
             console.log('Error in sending mail', err);
