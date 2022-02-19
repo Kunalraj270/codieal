99708dc8ca20ee44b2de8e78f6b6996c9d7c3451
Kunalraj270 <kunalraj2999@gmail.com>
2022-02-19 20:42:09 +0530

    inintal connction and join chat room create

---
+++ assets/js/chat_engine.js
@@ -0,0 +1,30 @@
+class ChatEngine{
+    constructor(chatBoxId , userEmail){
+        this.chatBox = $(`#${chatBoxId}`);
+        this.userEmail = userEmail;
+
+        this.socket  = io.connect('http://localhost:5000');
+
+        if(this.userEmail){
+            this.connectionHandler();
+        }
+    }
+
+    connectionHandler(){
+        let self = this;
+
+        this.socket.on('connect' , function(){
+            console.log('connection is  establised using socket...!');
+
+            self.socket.emit('join_room' , {
+                user_email : self.userEmail,
+                chatroom : 'Meta'
+            });
+
+
+            self.socket.on('user_joined' , function(data){
+                console.log('a user joined' , data);
+            })
+        });
+    }
+}
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,23 @@
 module.exports.chatSockets = function (socketServer){  
-    
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection' , function(socket){
+        console.log('new connection recevied' , socket.id);
+
+        socket.on('disconnect' , function(){
+            console.log('socket disconnected!')
+        });
+        
+        socket.on('join_room' , function(data){
+            console.log('joining reqest rec.' , data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined' , data);
+            
+        });
+
+
+
+    });
 }
