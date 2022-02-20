61b4fd1a2b2a2bb65ff5436513eda53fab4c2b90
Kunalraj270 <kunalraj2999@gmail.com>
2022-02-20 22:20:04 +0530

    send and recieved msg

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -14,9 +14,12 @@ module.exports.chatSockets = function (socketServer){
             socket.join(data.chatroom);
 
             io.in(data.chatroom).emit('user_joined' , data);
-            
-        });
 
+        });
+        // detect send msg and broadcast everyone
+        socket.on('send_message' , function(data){
+            io.in(data.chatroom).emit('receive_message', data)
+        });
 
 
     });
