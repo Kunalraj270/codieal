4f267c7686f732b549bb31f8aa3a0d348747ec7e
Kunalraj270 <kunalraj2999@gmail.com>
2022-08-12 01:59:04 +0530

    ++

--- config/passport-jwt-Strategy.js
+++ config/passport-jwt-Strategy.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const JWTStrategy = require('passport-jwt').Strategy;
 
-
 const ExtractJWT = require('passport-jwt').ExtractJwt;
 
 const User = require('../models/user');
